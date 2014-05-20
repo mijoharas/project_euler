9b1f4a11e901e9b2145b20c5e712459cbf4fe6b0
mike-work <michael.hauser-raspe@cantab.net>
2014-05-21 00:12:56 +0100

    go

--- go/2.go
+++ go/2.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func main() {
 	// lets put in some fibonnacci stuff here.
+	fmt.Println(find_fibonnacci_stuff())
+}
+
+func find_fibonnacci_stuff() int {
+	fib_old := 1
+	fib_new := 1
+	var total int
+	for fib_new < 4000000 {
+		fib_old, fib_new = fib_new, fib_old+fib_new
+		if fib_new%2 == 0 {
+			total += fib_new
+		}
+	}
+	return total
 }
