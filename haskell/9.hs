5b7085fb13118228241c2496991530380442a6a3
mike-work <michael.hauser-raspe@cantab.net>
2014-07-31 13:29:22 +0100

    haskell

--- haskell/9.hs
+++ haskell/9.hs
@@ -1,3 +1,4 @@
 -- very simplistic brute force list comprehension
-ans = [(x*y*z)| x <- [1..1000], y <- [1..1000], z <- [1..(1000 - x - y)], x**2 + y**2 == z**2, x + y + z == 1000]
+-- N.B. z must be taken from a list even with a single value.
+ans = [(x*y*z)| x <- [1..1000], y <- [1..(1000 - x)], z <- [(1000 - x - y)], x**2 + y**2 == z**2]
 main = print ans
