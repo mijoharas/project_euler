ef1cb7324af3b627db39179602690d786c4d670d
Michael Hauser-Raspe (Arch) <michael.hauser-raspe@cantab.net>
2014-08-09 22:00:09 +0100

    haskell 22

--- haskell/22.hs
+++ haskell/22.hs
@@ -4,4 +4,5 @@ import Data.List.Split
 myString = unsafePerformIO . readFile $ "names.txt"
 otherString = init $ tail $ myString
 names = splitOn "\",\"" otherString
+sortedList = sort names
 -- now we have the names
