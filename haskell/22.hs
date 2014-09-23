a2e75f9a529e295f0940ba01e82186f4e0fc8944
mike-work <michael.hauser-raspe@cantab.net>
2014-09-23 13:43:41 +0100

    project euler

--- haskell/22.hs
+++ haskell/22.hs
@@ -1,9 +1,13 @@
 import System.IO.Unsafe
 import Data.List.Split
+import Data.List
+import Data.Char
 
 myString = unsafePerformIO . readFile $ "names.txt"
 otherString = init $ tail $ myString
 names = splitOn "\",\"" otherString
 sortedList = sort names
+score =  sum . map (\char -> ord char - ord 'A' + 1)
+result = sum $ zipWith (\x y -> (score x) * y) sortedList [1..]
 -- now we have the names
 -- we then need to apply our function to these names
