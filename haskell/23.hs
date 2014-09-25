dc14ebfea3b2a53b63d4a469e5896b4e81f5f91c
mike-work <michael.hauser-raspe@cantab.net>
2014-09-25 19:16:16 +0100

    haskell 23

--- haskell/23.hs
+++ haskell/23.hs
@@ -1,5 +1,7 @@
 import Data.Numbers.Primes
+import Data.List
 import qualified Data.Map as M
+import qualified Data.Set as S
 
 isAbundant n = (prop_div_sum n) > n
 
@@ -15,7 +17,7 @@ divisors x = combinations list_of_nums
              divs = M.toList . (freqMap . primeFactors)
              freqMap x = M.fromListWith (+) [(num, 1) | num <- x]
 
--- from question 12
+-- from question 12 putting here so that they're both visible
 other_divisors = foldr ((*) . (+1)) 1 . map (snd) . divs
                   where
                     divs = M.toList . (freqMap . primeFactors)
@@ -23,7 +25,21 @@ other_divisors = foldr ((*) . (+1)) 1 . map (snd) . divs
 
 prop_div_sum x = (sum (divisors x)) - x
 
-abunList = filter isAbundant $ takeWhile (< 28123) [1..]
+limit = 28123
+
+abunList = filter isAbundant $ takeWhile (< limit) [1..]
 
 -- iterate through twice and make all sums then filter numbers that aren't
 -- in that list?
+testList = [1,2,2,3,4,5]
+deDup list = (S.toList . S.fromList) $ concat list
+listOfAbunSums = deDup $ map sumMapper $ tails abunList
+          where sumMapper [] = []
+                sumMapper (a:b) = sumMap a (a:b)
+                --create our sumMap function but stop once we get to the limit
+                sumMap a b = map (+ a) $ takeWhile (\x -> (x + a) <= limit) b
+-- this is like set difference
+difference = [1..limit] \\ listOfAbunSums
+result = sum difference
+
+-- kinda slow, good enough (chuck in binary search)
--- ruby/23.rb
+++ ruby/23.rb
@@ -13,18 +13,18 @@ factor_hash ={1 => 1}
 (2..28123).each {|x| factor_hash[x]=total_of_factors(x)}
 
 abundant_list=[]
-factor_hash.each {|k,v| if (k<v); abundant_list<<k;end}
+factor_hash.each {|k,v| if (k<v); abundant_list << k ;end}
 factor_of_2_abundants = abundant_list.combination(2).map {|x| x.reduce(:+)}
 
 #this takes too long, we're building a list that's large
 # and checking membership on each element, optimise with
 # binary tree, etc.
 ans_list = []
-(1..28123).each do |x| 
+(1..28123).each do |x|
 sums = abundant_list.take_while {|y| y<x}
 sums.each { |y|
  if sums.include?(x-y)
-  ans_list<<x
+  ans_list << x
   break
  end
 }
