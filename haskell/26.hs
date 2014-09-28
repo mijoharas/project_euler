c03d486f03a8bfc974f1640a52ded119b0ce8285
mike-work <michael.hauser-raspe@cantab.net>
2014-09-28 01:11:24 +0100

    haskell 26

--- haskell/26.hs
+++ haskell/26.hs
@@ -1,4 +1,9 @@
+import Data.List
 import Data.Numbers.Primes
-myList = filter (<1000) primes
-noCycles = head . filter ()
--- size_
+myList = takeWhile (<1000) primes
+noCycle n = headOrZero $ filter (\x -> 10^x `mod` n == 1 `mod` n) [1..n]
+headOrZero (a:as) = a
+headOrZero [] = 0
+secondList = map (\x -> [noCycle x, x]) myList
+result = maximumBy (\x y -> head x `compare` head y) secondList
+-- noCycles = head . filter ()
