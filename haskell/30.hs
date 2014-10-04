5a5a5fa2e9fea092a717d7a1ab20029bacb8f8c5
mike-work <michael.hauser-raspe@cantab.net>
2014-10-04 10:48:00 +0100

    haskell 30 finished

--- haskell/30.hs
+++ haskell/30.hs
@@ -1,3 +1,9 @@
 import Data.List
+import Data.Digits (digits)
 
-maxValue = fst $ head $ filter (\(a, b) -> a < b) $ zip (map (9**5*) [1..]) (map (10**) [1..])
+-- n is our power (for testing)
+maxValue n = fst $ head $ filter (\(a, b) -> a < b) $ zip (map (9^n*) [1..]) (map (10^) [1..])
+memoizePowers n = (map (^n) [0..9] !!)
+powerDigits n x = sum $ map (memoizePowers n) (digits 10 x)
+numSumFifthPowerDigits = filter (\n -> n == (powerDigits 5 n)) [2..(maxValue 5)]
+result = sum numSumFifthPowerDigits
