7ca017b23630c0b3c7001afff0b1bcf0e7840242
Michael Hauser-Raspe <hauserra@amazon.com>
2014-10-29 18:11:24 +0000

    haskell 51 (almost done)

---
+++ haskell/51.hs
@@ -0,0 +1,21 @@
+import Data.Digits
+import Data.Numbers.Primes
+import Data.List
+import qualified Data.Sequence as S
+-- Wow, lenses are crazy, still don't understand them
+import Control.Lens
+
+numberPrimeReplacements num replaceIndexes = length $ filter isPrime newNumbers
+  where newNumbers = map (unDigits 10 . replaceIndices replaceIndexes (digits 10 num)) (iterList replaceIndexes)
+        iterList rInd
+          | elem 0 rInd = [1..9]
+          | otherwise   = [0..9]
+
+
+-- this replaceIndices function is useful
+replaceIndices indices list num = over (elements (flip elem indices)) (const num) list
+
+indexPermutations num = tail $ subsequences [0..((length (digits 10 num)) - 1)]
+-- pretty damn slow but I learnt something so I'm happy with it
+result = head [x | x <- primes,
+               (maximum $ map (numberPrimeReplacements x) (indexPermutations x)) == 8]
