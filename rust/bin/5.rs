17cc55dbf1197381df8713deea5c8b6e2560d9c7
mike-work <michael.hauser-raspe@cantab.net>
2018-03-07 02:54:45 +0000

    Add 5.rs.

---
+++ rust/bin/5.rs
@@ -0,0 +1,12 @@
+#![feature(iterator_step_by)]
+
+pub fn main() {
+    let answer = (20..).step_by(20).skip_while(|num| {
+        !is_divisible_by_one_to_twenty(num)
+    }).take(1).collect::<Vec<u64>>();
+    println!("{:?}", answer.first().unwrap());
+}
+
+fn is_divisible_by_one_to_twenty(num: &u64) -> bool {
+    (2..20).all(|i| num % i == 0)
+}
